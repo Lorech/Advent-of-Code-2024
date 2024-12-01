3a819c110f485e7196d94bf69fde0e49d5a69f31
Lorech <laurisdedumets@gmail.com>
2024-12-01 15:24:35 +0200

    feat: Implement day one

--- pkg/puzzles/1.go
+++ pkg/puzzles/1.go
@@ -1,5 +1,12 @@
 package puzzles
 
+import (
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 // Provides the solution for day 1.
 //
 // In day 1, we must help The Historians find the location IDs of historically
@@ -11,5 +18,39 @@ package puzzles
 // values to get individual distances between locations, and then summing the
 // distances to get the total distance between the two lists.
 func DayOne(input string) int {
-	return 0
+	var left, right []int
+
+	// Parse both lists into separate integer slices.
+	rows := strings.Split(input, "\n")
+	for _, row := range rows {
+		ids := regexp.MustCompile(`\s+`).Split(row, -1)
+
+		first, error := strconv.Atoi(ids[0])
+		if error != nil {
+			panic(error)
+		}
+		second, error := strconv.Atoi(ids[1])
+		if error != nil {
+			panic(error)
+		}
+
+		left = append(left, first)
+		right = append(right, second)
+	}
+
+	// Sort both lists in ascending order.
+	sort.Ints(left)
+	sort.Ints(right)
+
+	// Calculate the total distance between the two lists.
+	distance := 0
+	for i := 0; i < len(left); i++ {
+		d := right[i] - left[i]
+		if d < 0 {
+			d = -d
+		}
+		distance += d
+	}
+
+	return distance
 }
