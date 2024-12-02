81ebe9db2459dccfc28776acb7a3f7712ded1972
Lorech <laurisdedumets@gmail.com>
2024-12-02 19:24:32 +0200

    fix: Remove references to content from Advent of Code

--- pkg/puzzles/1.go
+++ pkg/puzzles/1.go
@@ -7,22 +7,8 @@ import (
 	"strings"
 )
 
-// Provides the solution for day 1.
-//
-// In day 1, we must help The Historians find the location IDs of historically
-// significant locations where the Chief Historian could be found after disappearing.
-//
-// To do this, we must combine two individual lists of location IDs found by the
-// historians, and and the distances between each of their IDs. We can do this
-// by aligning the two lists of IDs from smallest to largest, subtracting their
-// values to get individual distances between locations, and then summing the
-// distances to get the total distance between the two lists.
-//
-// As a second form of comparison, we must find the similarity between the two
-// lists - maybe some of the locations are the same, indicating that they are
-// actually valid locations. We can do this by comparing the two lists of IDs,
-// multiplying each ID found in the left list with the amount of times is also
-// appears on the right list, repeating IDs included.
+// Day 1: Historian Hysteria
+// https://adventofcode.com/2024/day/1
 func DayOne(input string) (int, int) {
 	var left, right []int
 
