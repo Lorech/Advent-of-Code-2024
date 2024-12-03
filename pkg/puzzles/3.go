a62276c3198713680d0bb4f1fc95fe69c644366d
Lorech <laurisdedumets@gmail.com>
2024-12-03 18:24:57 +0200

    feat: Implement day 3, part 1

--- pkg/puzzles/3.go
+++ pkg/puzzles/3.go
@@ -1,7 +1,25 @@
 package puzzles
 
+import (
+	"regexp"
+	"strconv"
+)
+
 // Day 3: Mull It Over
 // https://adventofcode.com/2024/day/4
 func dayThree(input string) (int, int) {
-	return 0, 0
+	multiplication := 0
+
+	re := regexp.MustCompile(`(mul\(\d+?,\d+?\))`)
+	matches := re.FindAllString(input, -1)
+
+	for _, match := range matches {
+		reDigits := regexp.MustCompile(`\d+`)
+		digits := reDigits.FindAllString(match, -1)
+		first, _ := strconv.Atoi(digits[0])
+		second, _ := strconv.Atoi(digits[1])
+		multiplication += first * second
+	}
+
+	return multiplication, 0
 }
