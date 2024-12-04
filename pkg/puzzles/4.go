b28842f0cc30e62510f3c1bcf7d4d2d4f3003813
Lorech <laurisdedumets@gmail.com>
2024-12-04 17:17:35 +0200

    chore: Set up for day 4

---
+++ pkg/puzzles/4.go
@@ -0,0 +1,17 @@
+package puzzles
+
+// Day 4: Ceres Search
+// https://adventofcode.com/2024/day/4
+func dayFour(input string) (int, int) {
+	return d4p1(input), d4p2(input)
+}
+
+// Completes the first half of the puzzle for day 4.
+func d4p1(input string) int {
+	return 0
+}
+
+// Completes the second half of the puzzle for day 4.
+func d4p2(input string) int {
+	return 0
+}
